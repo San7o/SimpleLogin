12dffc60f7fbd66dd46099087cecf0d952876f2e
San7o <santigio2003@gmail.com>
2023-09-11 22:34:45 +0200

    Signup form

--- simpleLogin/simpleLoginApp/views.py
+++ simpleLogin/simpleLoginApp/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello I am working!")
+  return render(request, "simpleLoginApp/index.html")
+
+def signup(request):
+
+  if request.method == "POST":
+    username = request.POST.get('username')
+    fname = request.POST['fname'] # Also like this
+    lname = request.POST['lname']
+    email = request.POST['email']
+    pass1 = request.POST['pass1']
+    pass2 = request.POST['pass2']
+
+    myuser = User.objects.create_user(username, email, pass1)
+    myuser.first_name = fname
+    myuser.last_name = lname
+
+    myuser.save()
+    
+    messages.success(request, "Your account has been succesfully created. ")
+
+    return redirect("signin")
+
+
+  return render(request, "simpleLoginApp/signup.html")
+
+def signin(request):
+  return render(request, "simpleLoginApp/signin.html")
+
+def signout(request):
+  # return render(request, "simpleLoginApp/signout.html")
+  pass
